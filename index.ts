766e26c2ae34d5d63f80a68c30bcb9f6e2996f29
Bruce <holyblues@gmail.com>
2024-09-07 12:10:11 +0800

    Listing 4.94 Initial

--- index.ts
+++ index.ts
@@ -252,7 +252,7 @@ class Right implements Input {
 
 let playerx = 1;
 let playery = 1;
-let map: Tile[][] = [
+let rawMap: RawTile[][] = [
   [2, 2, 2, 2, 2, 2, 2, 2],
   [2, 3, 0, 1, 1, 2, 0, 2],
   [2, 4, 2, 6, 1, 2, 0, 2],
@@ -260,13 +260,52 @@ let map: Tile[][] = [
   [2, 4, 1, 1, 1, 9, 0, 2],
   [2, 2, 2, 2, 2, 2, 2, 2],
 ];
+let map: Tile2[][];
+function assertExhausted(x: never): never {
+  throw new Error("Unexpected object: " + x);
+}
+function transformTile(tile: RawTile) {
+  switch (tile) {
+    case RawTile.AIR: return new Air();
+    case RawTile.PLAYER: return new Player();
+    case RawTile.UNBREAKABLE: return new Unbreakable();
+    case RawTile.STONE: return new Stone();
+    case RawTile.FALLING_STONE: return new FallingStone();
+    case RawTile.BOX: return new Box();
+    case RawTile.FALLING_BOX: return new FallingBox();
+    case RawTile.FLUX: return new Flux();
+    case RawTile.KEY1: return new Key1();
+    case RawTile.LOCK1: return new Lock1();
+    case RawTile.KEY2: return new Key2();
+    case RawTile.LOCK2: return new Lock2();
+    default: assertExhausted(tile);
+  }
+}
+function transformMap() {
+  map = new Array(rawMap.length);
+  for (let y = 0; y < rawMap.length; y++) {
+    map[y] = new Array(rawMap[y].length);
+    for (let x = 0; x < rawMap[y].length; x++) {
+      map[y][x] = transformTile(rawMap[y][x]);
+    }
+  }
+}
 
 let inputs: Input[] = [];
 
-function remove(tile: Tile) {
+function removeLock2() {
   for (let y = 0; y < map.length; y++) {
     for (let x = 0; x < map[y].length; x++) {
-      if (map[y][x] === tile) {
+      if (map[y][x].isLock2()) {
+        map[y][x] = new Air();
+      }
+    }
+  }
+}
+function removeLock1() {
+  for (let y = 0; y < map.length; y++) {
+    for (let x = 0; x < map[y].length; x++) {
+      if (map[y][x].isLock1()) {
         map[y][x] = new Air();
       }
     }
@@ -291,10 +330,10 @@ function moveHorizontal(dx: number) {
     map[playery][playerx + dx + dx] = map[playery][playerx + dx];
     moveToTile(playerx + dx, playery);
   } else if (map[playery][playerx + dx].isKey1()) {
-    remove(new Lock1());
+    removeLock1();
     moveToTile(playerx + dx, playery);
   } else if (map[playery][playerx + dx].isKey2()) {
-    remove(new Lock2());
+    removeLock2();
     moveToTile(playerx + dx, playery);
   }
 }
@@ -304,10 +343,10 @@ function moveVertical(dy: number) {
     || map[playery + dy][playerx].isAir()) {
     moveToTile(playerx, playery + dy);
   } else if (map[playery + dy][playerx].isKey1()) {
-    remove(new Lock1());
+    removeLock1();
     moveToTile(playerx, playery + dy);
   } else if (map[playery + dy][playerx].isKey2()) {
-    remove(new Lock2());
+    removeLock2();
     moveToTile(playerx, playery + dy);
   }
 }
@@ -403,6 +442,7 @@ function gameLoop() {
 }
 
 window.onload = () => {
+  transformMap();
   gameLoop();
 }
 
