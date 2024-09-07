5b1d5bf1ac8f1c4646ab558145102f2f294d426b
Bruce <holyblues@gmail.com>
2024-09-07 12:13:41 +0800

    remove duplicate interface

--- index.ts
+++ index.ts
@@ -14,7 +14,8 @@ enum RawTile {
   KEY2, LOCK2
 }
 
-interface Tile2 {
+
+interface Tile {
   isAir(): boolean;
   isFlux(): boolean;
   isUnbreakable(): boolean;
@@ -29,22 +30,7 @@ interface Tile2 {
   isLock2(): boolean;
 }
 
-interface Tile2 {
-  isAir(): boolean;
-  isFlux(): boolean;
-  isUnbreakable(): boolean;
-  isPlayer(): boolean;
-  isStone(): boolean;
-  isFallingStone(): boolean;
-  isBox(): boolean;
-  isFallingBox(): boolean;
-  isKey1(): boolean;
-  isLock1(): boolean;
-  isKey2(): boolean;
-  isLock2(): boolean;
-}
-
-class Air implements Tile2 {
+class Air implements Tile {
   isAir() { return true; }
   isFlux() { return false; }
   isUnbreakable() { return false; }
@@ -59,7 +45,7 @@ class Air implements Tile2 {
   isLock2() { return false; }
 }
 
-class Flux implements Tile2 {
+class Flux implements Tile {
   isAir() { return false; }
   isFlux() { return true; }
   isUnbreakable() { return false; }
@@ -74,7 +60,7 @@ class Flux implements Tile2 {
   isLock2() { return false; }
 }
 
-class Unbreakable implements Tile2 {
+class Unbreakable implements Tile {
   isAir() { return false; }
   isFlux() { return false; }
   isUnbreakable() { return true; }
@@ -89,7 +75,7 @@ class Unbreakable implements Tile2 {
   isLock2() { return false; }
 }
 
-class Player implements Tile2 {
+class Player implements Tile {
   isAir() { return false; }
   isFlux() { return false; }
   isUnbreakable() { return false; }
@@ -104,7 +90,7 @@ class Player implements Tile2 {
   isLock2() { return false; }
 }
 
-class Stone implements Tile2 {
+class Stone implements Tile {
   isAir() { return false; }
   isFlux() { return false; }
   isUnbreakable() { return false; }
@@ -119,7 +105,7 @@ class Stone implements Tile2 {
   isLock2() { return false; }
 }
 
-class FallingStone implements Tile2 {
+class FallingStone implements Tile {
   isAir() { return false; }
   isFlux() { return false; }
   isUnbreakable() { return false; }
@@ -134,7 +120,7 @@ class FallingStone implements Tile2 {
   isLock2() { return false; }
 }
 
-class Box implements Tile2 {
+class Box implements Tile {
   isAir() { return false; }
   isFlux() { return false; }
   isUnbreakable() { return false; }
@@ -149,7 +135,7 @@ class Box implements Tile2 {
   isLock2() { return false; }
 }
 
-class FallingBox implements Tile2 {
+class FallingBox implements Tile {
   isAir() { return false; }
   isFlux() { return false; }
   isUnbreakable() { return false; }
@@ -164,7 +150,7 @@ class FallingBox implements Tile2 {
   isLock2() { return false; }
 }
 
-class Key1 implements Tile2 {
+class Key1 implements Tile {
   isAir() { return false; }
   isFlux() { return false; }
   isUnbreakable() { return false; }
@@ -179,7 +165,7 @@ class Key1 implements Tile2 {
   isLock2() { return false; }
 }
 
-class Lock1 implements Tile2 {
+class Lock1 implements Tile {
   isAir() { return false; }
   isFlux() { return false; }
   isUnbreakable() { return false; }
@@ -194,7 +180,7 @@ class Lock1 implements Tile2 {
   isLock2() { return false; }
 }
 
-class Key2 implements Tile2 {
+class Key2 implements Tile {
   isAir() { return false; }
   isFlux() { return false; }
   isUnbreakable() { return false; }
@@ -209,7 +195,7 @@ class Key2 implements Tile2 {
   isLock2() { return false; }
 }
 
-class Lock2 implements Tile2 {
+class Lock2 implements Tile {
   isAir() { return false; }
   isFlux() { return false; }
   isUnbreakable() { return false; }
@@ -260,7 +246,7 @@ let rawMap: RawTile[][] = [
   [2, 4, 1, 1, 1, 9, 0, 2],
   [2, 2, 2, 2, 2, 2, 2, 2],
 ];
-let map: Tile2[][];
+let map: Tile[][];
 function assertExhausted(x: never): never {
   throw new Error("Unexpected object: " + x);
 }
